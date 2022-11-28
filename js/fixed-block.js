79436be4fc329a5f08408ef6fb157ff09bd4efaf
pokemzed <60814385+pokemzed@users.noreply.github.com>
2022-11-28 16:32:27 +0300

    №2
    create grid&fixed-block scroll

---
+++ js/fixed-block.js
@@ -0,0 +1,32 @@
+const fixedBlock = document.querySelector('.filters-price'),
+    filters = document.querySelector('.filters'),
+    container = document.querySelector('.container'),
+    offsetLeft = container.offsetLeft + 15,
+    filtersOffsetTop = filters.offsetTop,
+    filtersWidth = filters.offsetWidth,
+    smallOffset = 20
+
+    console.log(filtersWidth)
+
+const fixedScrollBlock = () => {
+    let scrollDistance = window.scrollY
+    
+    if(scrollDistance > (filtersOffsetTop - smallOffset) && scrollDistance <= (filters.offsetHeight + filtersOffsetTop)){
+        fixedBlock.style.left = `${offsetLeft}px`
+        fixedBlock.style.width = `${filtersWidth}px`
+        fixedBlock.classList.remove('absolute')
+        fixedBlock.classList.add('fixed')
+    } else{
+        fixedBlock.style.left = `0px`
+        fixedBlock.style.width = `100%`
+        fixedBlock.classList.remove('fixed')
+    }
+
+    if(scrollDistance >= (filtersOffsetTop - smallOffset) + filters.offsetHeight - fixedBlock.offsetHeight){
+        fixedBlock.classList.add('absolute')
+        fixedBlock.style.left = `0px`
+        fixedBlock.style.width = `100%`
+        fixedBlock.classList.remove('fixed')
+    }
+}
+window.addEventListener('scroll', fixedScrollBlock)
